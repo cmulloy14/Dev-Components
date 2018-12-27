6acd5017d5cef108ec796b5a46ddc99a35c9cb3f
Charles Mulloy <charles.mulloy@etrade.com>
2018-12-26 20:14:26 -0600

    Initial Commit

---
+++ Dev Components/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dev Components
+//
+//  Created by Mulloy, Charles on 12/26/18.
+//  Copyright © 2018 Pat Mulloy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Dev ComponentsTests/Dev_ComponentsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dev_ComponentsTests.swift
+//  Dev ComponentsTests
+//
+//  Created by Mulloy, Charles on 12/26/18.
+//  Copyright © 2018 Pat Mulloy. All rights reserved.
+//
+
+import XCTest
+@testable import Dev_Components
+
+class Dev_ComponentsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
